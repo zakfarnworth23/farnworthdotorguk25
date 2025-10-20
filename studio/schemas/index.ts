ae7a32ff290695bbfcf806ce48d89cffe6b06f2e
Zak Farnworth <130570087+zakfarnworth23@users.noreply.github.com>
2025-10-20 15:14:51 +0100

    Delete studio directory

--- studio/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import blogPost from './blogPost'
-import pressStatement from './pressStatement'
-import announcement from './announcement'
-
-export default [blogPost, pressStatement, announcement]
