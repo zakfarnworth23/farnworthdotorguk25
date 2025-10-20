2a76679058f16c75a57204c8739b78ebefb5f833
Zak Farnworth <130570087+zakfarnworth23@users.noreply.github.com>
2025-10-20 12:00:23 +0100

    Create index.ts

---
+++ studio/schemas/index.ts
@@ -0,0 +1,5 @@
+import blogPost from './blogPost'
+import pressStatement from './pressStatement'
+import announcement from './announcement'
+
+export default [blogPost, pressStatement, announcement]
